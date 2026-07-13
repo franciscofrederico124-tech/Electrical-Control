0922edb0125c04cbd64c66812b9e96b9b216ae8e
franciscofrederico124-tech <franciscofrederico124@gmail.com>
2026-07-13 18:17:48 +0100

    Version final

--- firmware/src/hooks/global.h
+++ firmware/src/hooks/global.h
@@ -1,11 +1,13 @@
 #pragma once
 #include <Arduino.h>
 
-const String ssid = "itel A100C";
-const String password = "francisco_tech_2026";
-const String apiBase = "http://192.168.149.103:2001";
+const String ssid = "Projecto anual";
+const String password = "Projecto anual";
+const String apiBase = "https://electrical-control.onrender.com";
+int servo_angle = 0;
 
 #define v_sensor_pin 35
 #define servo_pin 13
 #define green_pin 12
 #define yellow_pin 14
+#define ldr_pin 34
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -10,6 +10,7 @@
 #include "time.h"
 
 Sensor voltage_sensor;
+Sensor ldr;
 Servo solar_servo;
 Actor green;
 Actor yellow;
@@ -22,6 +23,7 @@ void setup()
 {
   Serial.begin(115200);
   voltage_sensor.begin(v_sensor_pin);
+  ldr.begin(ldr_pin);
   green.begin(green_pin);
   yellow.begin(yellow_pin);
   green.low();
@@ -39,35 +41,8 @@ void setup()
 
 void loop()
 {
-  struct tm time_info;
-  time_t epoch_time;
-  time(&epoch_time);
 
-  bool is_time_valid = getLocalTime(&time_info);
-  int servo_angle = 0;
-
-  if (is_time_valid)
-  {
-    int current_hour = time_info.tm_hour;
-    int current_minute = time_info.tm_min;
-
-    int total_minutes_today = (current_hour * 60) + current_minute;
-    int sunrise_minutes = 6 * 60;
-    int sunset_minutes = 18 * 60;
-
-    if (total_minutes_today >= sunrise_minutes && total_minutes_today <= sunset_minutes)
-    {
-      servo_angle = map(total_minutes_today, sunrise_minutes, sunset_minutes, 0, 180);
-    }
-    else
-    {
-      servo_angle = 0;
-    }
-  }
-
-  solar_servo.write(servo_angle);
-
-  const int voltage_value = ((int)(round((int)(voltage_sensor.read_analog() * 25) / 4095 )));
+  const int voltage_value = ((int)(round((int)(voltage_sensor.read_analog() * 25) / 4095)));
 
   if (voltage_value < 1)
   {
@@ -88,21 +63,7 @@ void loop()
   doc["data"]["i"] = String(2) + "A";
   doc["data"]["P"] = String((int)round(voltage_value * 2)) + "W";
 
-  if (is_time_valid)
-  {
-    char formatted_date[12];
-    char formatted_time[9];
-    sprintf(formatted_date, "%02d/%02d/%04d", time_info.tm_mday, time_info.tm_mon + 1, time_info.tm_year + 1900);
-    sprintf(formatted_time, "%02d:%02d:%02d", time_info.tm_hour, time_info.tm_min, time_info.tm_sec);
-
-    doc["time"]["date"] = formatted_date;
-    doc["time"]["hour"] = formatted_time;
-    doc["time"]["epoch"] = (unsigned long)epoch_time;
-  }
-  else
-  {
-    doc["time"]["status"] = "NTP synchronization error";
-  }
+  doc["time"]["status"] = "NTP synchronization error";
 
   String json_payload;
   serializeJsonPretty(doc, json_payload);
